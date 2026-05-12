33ddb745a177eb063fdb5274f83e4c4e9f28da61
Fajar Nur Prasetyo <fajarnurprasetyo.2@gmail.com>
2026-05-12 18:30:29 +0700

    feat: refactor proxy to handle bot requests and integrate auth functionality

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,1 +1,12 @@
-export { auth as proxy } from "./libs/auth";
+import {
+  NextResponse
+} from "next/server";
+import { auth } from "./libs/auth";
+
+const botRegEx =
+  /facebookexternalhit|Facebot|Twitterbot|Slackbot|Discordbot|LinkedInBot|WhatsApp/i;
+
+export default auth((req) => {
+  const ua = req.headers.get("user-agent") || "";
+  if (botRegEx.test(ua)) return NextResponse.next();
+});
